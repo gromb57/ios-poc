a8189cc112fcd557495ad7880e61e396e22de7cc
Bach Jérôme <jeromeb57@gmail.com>
2023-06-06 23:12:41 +0200

    spatial

---
+++ ios-poc/ios-poc/spatial/spatial.swift
@@ -0,0 +1,18 @@
+//
+//  spatial.swift
+//  ios-poc
+//
+//  Created by Jerome Bach on 06/06/2023.
+//
+
+import Foundation
+
+/// Note about spatials
+///
+/// App icons : up to 3 layers for 3d effect
+///
+/// Interactive elements(like buttons) : default size of interactive is 44x44 pt, for spatial it needs 60x60, so we need to add 8pt of space on each side
+///
+/// Elements must have at least 4pt of space between them
+///
+/// Sheets/modal : close button on top-left corner
